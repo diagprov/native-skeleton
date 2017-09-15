a6d785f261452521a0b6c61a270c63945b54cc5b
Antony Vennard <antony@vennard.ch>
2017-09-15 12:22:51 +0200

    Support latest WDK.

--- vs/nativeskel.c
+++ vs/nativeskel.c
@@ -15,13 +15,23 @@ typedef struct {
 
 NTSTATUS NTAPI NtTerminateProcess(HANDLE ProcessHandle, LONG ExitStatus);
 NTSTATUS NTAPI NtDisplayString(PUNICODE_STRING String);
+NTSYSAPI
+NTSTATUS
+NTAPI
+NtDelayExecution(
+	IN BOOLEAN              Alertable,
+	IN PLARGE_INTEGER       DelayInterval);
 
 void NtProcessStartup(PSTARTUP_ARGUMENT Argument)
 {
     UNICODE_STRING usMessage;
 
     UNREFERENCED_PARAMETER(Argument);
+    LARGE_INTEGER li = { 0 };
+    li.LowPart = 100000000;
+
     RtlInitUnicodeString(&usMessage, L"Hello, world.");
     NtDisplayString(&usMessage);
+    NtDelayExecution(TRUE, &li);
     NtTerminateProcess(NtCurrentProcess(), 0);
 }
